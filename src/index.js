68b2ab43c429f0bfabaf5f18b5a9f922fa65e093
maliza constantine <malizacoco4@gmail.com>
2020-11-07 11:47:23 +0200

    add components for todolist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ToDoList from './components/ToDoList'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToDoList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
